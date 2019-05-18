1cec135336a9aa2f3885166ce736fafbc62dd9ee
bubblefoil <go.holyal@gmail.com>
2019-05-19 01:18:07 +0200

    Add basic blinkers

--- arduino/mech-ctrl/mech-ctrl.ino
+++ arduino/mech-ctrl/mech-ctrl.ino
@@ -4,17 +4,25 @@
 #define NUM_LEDS 144
 // Index of the first LED of the tail light
 #define REAR_LEDS_START 72
+#define LEDS_FL 0
+#define LEDS_FR 36
+#define LEDS_BL 72
+#define LEDS_BR 108
 // Controls the LED strip
 #define DATA_PIN 6
+
+//Blinker light hue in the range from 0 - red to 255 - red again
+#define BLINKER_HUE 24
 
 #define RX 11
 #define TX 10
 // Digital input pins for buttons
-#define pinIN1 2
-#define pinIN2 4
+#define pinLightMode 2
+#define pinTurnLeft 4
+#define pinTurnRight 7
 
 // Light modes
-// Number of modes. For modulo division when cycling through the modes.
+// Number of light modes. For modulo division when cycling through the modes.
 const byte N_MODES = 4;
 const byte OFF = 0;
 const byte DAY = 1;
@@ -23,6 +31,11 @@ const byte PARK = 3;
 
 byte lightMode = OFF;
 byte lastLightMode = OFF;
+
+//Left blinker
+const byte LEFT = 11;
+//Right blinker
+const byte RIGHT = 12;
 
 // Number of head/tail light LEDs. These shine at full brightness.
 const byte N_FRONTAL = 6;
@@ -46,23 +59,34 @@ int buttonState1 = 0;
 // previous state of the button
 int lastButtonState1 = 0;
 
+// Total duration of a single ON/OFF blink in milliseconds.
+const unsigned long turnSignalInterval = 1000L;
+// Elapsed time since switching a blinker on.
+unsigned long blinkerSwitchOnTime = 0L;
+bool lastBlinkerLightState = false;
+
+// Forward prototype declaration because of the default parameter.
+void updateLights(bool show = true);
+
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Arduino ON.");
 
   // Setup button pins
-  pinMode(pinIN1, INPUT);
-  pinMode(pinIN2, INPUT);
+  pinMode(pinLightMode, INPUT);
+  pinMode(pinTurnLeft, INPUT);
+  pinMode(pinTurnRight, INPUT);
 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
   updateLights();
 }
 
 void loop()
 {
   // Detect a button press by checking for its state change
-  buttonState1 = digitalRead(pinIN1);
+  buttonState1 = digitalRead(pinLightMode);
   if (buttonState1 != lastButtonState1)
   {
     if (buttonState1 == HIGH)
@@ -83,9 +107,94 @@ void loop()
     // Add some stability
     delay(200);
   }
+
+  handleBlinkers();
 }
 
-void updateLights()
+void handleBlinkers()
+{
+  byte signal = 0;
+  if (digitalRead(pinTurnLeft) == HIGH)
+  {
+    signal = LEFT;
+    // blink(LEFT);
+    frontLeft(CHSV(32, 255, 255));
+    rearLeft(CHSV(32, 255, 255));
+  }
+  else if (digitalRead(pinTurnRight) == HIGH)
+  {
+    signal = RIGHT;
+  }
+  else
+  {
+    if (blinkerSwitchOnTime != 0)
+    {
+      // Reset to the state before blinker
+      updateLights();
+    }
+    blinkerSwitchOnTime = 0;
+  }
+
+  if (signal != 0)
+  {
+    if (blinkerSwitchOnTime == 0)
+    {
+      // Init timer after the first button press detection
+      blinkerSwitchOnTime = millis();
+    }
+    blink(signal);
+  }
+}
+
+/**
+ * Checks the timer whether the turn signal state is on or off.
+ */
+bool isBlinkerLightOn()
+{
+  unsigned long elapsed = (millis() - blinkerSwitchOnTime) % turnSignalInterval;
+  // return (elapsed < (turnSignalInterval >> 1));
+  return true;
+}
+
+/**
+ * Returns true if we are in the first half of the blinker interval,
+ * measured since the blinker was switched on.
+ */
+bool getBlinkerLightState(unsigned long switchOnTime)
+{
+  unsigned long elapsed = (millis() - switchOnTime);
+  // Less than half the blinker interval elapsed?
+  return (elapsed % turnSignalInterval < (turnSignalInterval / 2));
+}
+
+void blink(byte side)
+{
+  //Is the blinker light on or off at the moment?
+  bool blinkerState = getBlinkerLightState(blinkerSwitchOnTime);
+
+  if (!blinkerState)
+  {
+    updateLights(false);
+  }
+
+  byte brightness = blinkerState ? 255 : 0;
+  CHSV blinkerColor = CHSV(BLINKER_HUE, 255, brightness);
+  if (side == RIGHT)
+  {
+    frontRight(blinkerColor);
+    rearRight(blinkerColor);
+  }
+  else if (side == LEFT)
+  {
+    frontLeft(blinkerColor);
+    rearLeft(blinkerColor);
+  }
+  FastLED.show();
+
+  lastBlinkerLightState = blinkerState;
+}
+
+void updateLights(bool show)
 {
   switch (lightMode)
   {
@@ -103,7 +212,10 @@ void updateLights()
     off();
     break;
   }
-  FastLED.show();
+  if (show)
+  {
+    FastLED.show();
+  }
 }
 
 void off()
@@ -111,20 +223,34 @@ void off()
   FastLED.clear(false);
 }
 
+void frontLeft(const CHSV &col)
+{
+  fill_solid(leds, LEDS_FR, col);
+}
+
+void frontRight(const CHSV &col)
+{
+  fill_solid(&leds[LEDS_FR], REAR_LEDS_START - LEDS_FR, col);
+}
+
+void rearLeft(const CHSV &col)
+{
+  fill_solid(&leds[LEDS_BL], LEDS_BR - LEDS_BL, col);
+}
+
+void rearRight(const CHSV &col)
+{
+  fill_solid(&leds[LEDS_BR], NUM_LEDS - LEDS_BR, col);
+}
+
 /**
- * Activates night light mode. Dim light on sides, full brightness on nose and tail.
+ * Activates parking lights mode. Dim red/white lights all around.
 */
 void park()
 {
   off();
-  for (int led = 0; led < REAR_LEDS_START; led++)
-  {
-    leds[led] = CHSV(32, 20, 10);
-  }
-  for (int led = REAR_LEDS_START; led < NUM_LEDS; led++)
-  {
-    leds[led] = CHSV(0, 255, 15);
-  }
+  fill_solid(leds, REAR_LEDS_START, CHSV(32, 20, 8));
+  fill_solid(&leds[REAR_LEDS_START], NUM_LEDS - REAR_LEDS_START, CHSV(0, 255, 12));
 }
 
 /**
@@ -141,14 +267,8 @@ void day()
 */
 void night()
 {
-  for (int led = 0; led < REAR_LEDS_START; led++)
-  {
-    leds[led] = 0x0a0a0a;
-  }
-  for (int led = REAR_LEDS_START; led < NUM_LEDS; led++)
-  {
-    leds[led] = 0x330000;
-  }
+  fill_solid(leds, REAR_LEDS_START, CHSV(0, 0, 15));
+  fill_solid(&leds[REAR_LEDS_START], NUM_LEDS - REAR_LEDS_START, CHSV(0, 255, 15));
   frontalArea();
 }
 
